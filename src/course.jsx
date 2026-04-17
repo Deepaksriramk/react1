2e3a3cd49ad90c22efe2e48fb6eae5618cbba8b7
Deepaksriramk <sriram934596@gmail.com>
2026-04-17 21:11:59 +0530

    initial commit

--- src/course.jsx
+++ src/course.jsx
@@ -1,14 +1,23 @@
 function Course({ name, image, price, rating }) {
   return (
-    <>
-      <div className="card">
-        <img src={image} alt={name} />
-        <h3>{name}</h3>
-        <h4>{price}</h4>
-        <h5>{rating}</h5>
-        <button>Buy Now</button>
-      </div>
-    </>
+      <>
+        <div className="card">
+          <img src={image} alt={name} />
+          <h3>{name}</h3>
+          <h4>{price}</h4>
+          <h5>{rating}</h5>
+          <button onClick={() => {
+            const result = confirm("Do you want to buy this course?");
+            if (result) {
+              alert(name+"course purcheased");
+            } else {
+              alert("You clicked No");
+            }
+          }}>
+            Buy Now
+          </button>
+        </div>
+      </>
   );
 }
 
