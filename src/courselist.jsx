4a7eb701114386b6a7a35d2dd795ba5c1c05579b
Deepaksriramk <sriram934596@gmail.com>
2026-04-20 22:37:57 +0530

    addition of json

--- src/courselist.jsx
+++ src/courselist.jsx
@@ -8,43 +8,7 @@ import { useState ,useEffect} from "react";
 
 function CourseList() {
 
-  const [courses, setCourse] = useState([
-    {
-      id: 1,
-      name: "HTML full course",
-      price: 1213,
-      rating: "⭐ 4.0",
-      image: HTML
-    },
-    {
-      id: 2,
-      name: "CSS full course",
-      price: 1243,
-      rating: "⭐ 4.0",
-      image: CSS
-    },
-    {
-      id: 3,
-      name: "JS full course",
-      price: 1223,
-      rating: "⭐ 4.0",
-      image: JS
-    },
-    {
-      id: 4,
-      name: "JSX full course",
-      price: 1263,
-      rating: "⭐ 4.0",
-      image: JSX
-    },
-    {
-      id: 5,
-      name: "ANGULAR full course",
-      price: 1273,
-      rating: "⭐ 4.0",
-      image: ANGULAR
-    }
-  ]);
+  const [courses, setCourse] = useState();
 
   function handleDelete(id) {
     const filteredCourses = courses.filter(
